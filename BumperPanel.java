d47793f184986644e226c8e927fedea3bde96e87
mrabs <abdallaosman6@gmail.com>
2015-08-03 09:46:56 -0400

    changes

--- BumperPanel.java
+++ BumperPanel.java
@@ -23,105 +23,105 @@ public class BumperPanel extends JPanel {
    private Map map;
 
    public BumperPanel(int x, int y) {
-  	xFRAME = x;
-  	yFRAME = y;
-  	myImage = new BufferedImage(xFRAME, yFRAME, BufferedImage.TYPE_INT_RGB);
-  	myBuffer = myImage.getGraphics();
-  	map = new Map();
-  	bluePixels = map.getArray();
-  	pacman = new Pacman(200, 200);
- 	 
-  	ghosts = new Ghost[5];
-  	for(int i = 0; i < ghosts.length; i++) {
-     	ghosts[i] = new Ghost();
-  	}
-  	// ghosts[1] = new Ghost(260, 140);
+      xFRAME = x;
+      yFRAME = y;
+      myImage = new BufferedImage(xFRAME, yFRAME, BufferedImage.TYPE_INT_RGB);
+      myBuffer = myImage.getGraphics();
+      map = new Map();
+      bluePixels = map.getArray();
+      pacman = new Pacman(200, 200);
+    
+      ghosts = new Ghost[5];
+      for(int i = 0; i < ghosts.length; i++) {
+         ghosts[i] = new Ghost();
+      }
+   // ghosts[1] = new Ghost(260, 140);
    //   	ghosts[2] = new Ghost(240, 160);
    //   	ghosts[3] = new Ghost(250, 150);
    //   	ghosts[0] = new Ghost(240, 140);
- 	 
-  	refresher = new Timer(1, new Refresh());
+    
+      refresher = new Timer(1, new Refresh());
    
-  	refresher.start();
-  	setFocusable(true);
-  	addKeyListener(new Key());
+      refresher.start();
+      setFocusable(true);
+      addKeyListener(new Key());
    
    }
    
    public static void setLives(int x)
    {
-  	//x = Integer.parseInt(JOptionPane.showInputDialog("Set Lives: "));
-  	lives = x;
+   //x = Integer.parseInt(JOptionPane.showInputDialog("Set Lives: "));
+      lives = x;
    }
    
    public static int getLives()
    {
-  	return lives;
+      return lives;
    }
 
    public void paintComponent(Graphics g) {
-  	g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
+      g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
    }
 
    public class Key extends KeyAdapter {
    
-  	public void keyPressed(KeyEvent e) {
-     	//System.out.println(e.getKeyCode());
-     	if ((e.getKeyCode() == KeyEvent.VK_UP) && (pacman.getY() > 15)) {
-        	pacman.moveVertical(-5);
-     	}
-     	if ((e.getKeyCode() == KeyEvent.VK_DOWN) && (pacman.getY() < 285)) {
-        	pacman.moveVertical(5);
-     	}
-     	if ((e.getKeyCode() == KeyEvent.VK_RIGHT) && (pacman.getX() < 485)) {
-        	pacman.moveHorizontal(5);
-     	}
-     	if ((e.getKeyCode() == KeyEvent.VK_LEFT) && (pacman.getX() > 15)) {
-        	pacman.moveHorizontal(-5);
-     	}
-  	}
+      public void keyPressed(KeyEvent e) {
+      //System.out.println(e.getKeyCode());
+         if ((e.getKeyCode() == KeyEvent.VK_UP) && (pacman.getY() > 15)) {
+            pacman.moveVertical(-5);
+         }
+         if ((e.getKeyCode() == KeyEvent.VK_DOWN) && (pacman.getY() < 285)) {
+            pacman.moveVertical(5);
+         }
+         if ((e.getKeyCode() == KeyEvent.VK_RIGHT) && (pacman.getX() < 485)) {
+            pacman.moveHorizontal(5);
+         }
+         if ((e.getKeyCode() == KeyEvent.VK_LEFT) && (pacman.getX() > 15)) {
+            pacman.moveHorizontal(-5);
+         }
+      }
    }
 
    private class Refresh implements ActionListener {
    
-  	public void actionPerformed(ActionEvent e) {
-     	myBuffer.setColor(Color.black);
-     	myBuffer.fillRect(0, 0, xFRAME, yFRAME);
- 	 
-     	myBuffer.drawImage(map.getImage(), 0, 0, xFRAME, yFRAME, null);
- 	 
-     	pacman.draw(myBuffer);
-     	WallCollision.checkNorth(pacman, bluePixels);
-     	WallCollision.checkEast(pacman, bluePixels);
-     	WallCollision.checkSouth(pacman, bluePixels);
-     	WallCollision.checkWest(pacman, bluePixels);
-    	 
-     	if(slowCount == 7) {
-        	for (int i = 0; i < ghosts.length; i++) {
-           	ghosts[i].draw(myBuffer);
-           	WallCollision.checkNorth(ghosts[i], bluePixels);
-           	WallCollision.checkEast(ghosts[i], bluePixels);
-           	WallCollision.checkSouth(ghosts[i], bluePixels);
-           	WallCollision.checkWest(ghosts[i], bluePixels);
-           	ghosts[i].move(1);
-           	if (DeathByGhost.collide(pacman, ghosts[i])) {
-              	refresher.stop();
-              	GameOver.endGame();
-           	}
-        	}
-        	slowCount = 0;
-     	}
-     	slowCount++;
-    	 
-     	for(int i = 0; i < ghosts.length; i++)
-        	ghosts[i].draw(myBuffer);
+      public void actionPerformed(ActionEvent e) {
+         myBuffer.setColor(Color.black);
+         myBuffer.fillRect(0, 0, xFRAME, yFRAME);
+      
+         myBuffer.drawImage(map.getImage(), 0, 0, xFRAME, yFRAME, null);
+      
+         pacman.draw(myBuffer);
+         WallCollision.checkNorth(pacman, bluePixels);
+         WallCollision.checkEast(pacman, bluePixels);
+         WallCollision.checkSouth(pacman, bluePixels);
+         WallCollision.checkWest(pacman, bluePixels);
+       
+         if(slowCount == 7) {
+            for (int i = 0; i < ghosts.length; i++) {
+               ghosts[i].draw(myBuffer);
+               WallCollision.checkNorth(ghosts[i], bluePixels);
+               WallCollision.checkEast(ghosts[i], bluePixels);
+               WallCollision.checkSouth(ghosts[i], bluePixels);
+               WallCollision.checkWest(ghosts[i], bluePixels);
+               ghosts[i].move(1);
+               if (DeathByGhost.collide(pacman, ghosts[i])) {
+                  refresher.stop();
+                  GameOver.endGame();
+               }
+            }
+            slowCount = 0;
+         }
+         slowCount++;
+       
+         for(int i = 0; i < ghosts.length; i++)
+            ghosts[i].draw(myBuffer);
        	 
-     	myBuffer.setColor(Color.black);
-     	myBuffer.setFont(new Font("Monospaced", Font.BOLD, 8));
-     	myBuffer.drawString("Lives:" + lives, 420, 60);
-    	 
-     	repaint();
-  	}
+         myBuffer.setColor(Color.black);
+         myBuffer.setFont(new Font("Monospaced", Font.BOLD, 8));
+         myBuffer.drawString("Lives:" + lives, 420, 60);
+       
+         repaint();
+      }
    }
 
 }
--- Start.java
+++ Start.java
@@ -2,14 +2,15 @@ import
 javax.swing.*;
 
 public class Start {
-public static JFrame menuFrame;
-public static void main(String[] args)
-{
-menuFrame = new JFrame("PACMAN");
-menuFrame.setSize(300, 400);
-menuFrame.setLocation(200, 200);
-menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-menuFrame.setContentPane(new MenuPanel());
-menuFrame.setVisible(true);
-}
+   public static JFrame menuFrame;
+   public static void main(String[] args)
+   {
+      menuFrame = new JFrame("PACMAN");
+      menuFrame.setSize(300, 400);
+      menuFrame.setLocation(200, 200);
+      menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      menuFrame.setContentPane(new MenuPanel());
+      menuFrame.setVisible(true);
+      System.out.println("test");
+   }
 }
