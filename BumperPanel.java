a57f9ece0a68939b9a7a8bb62eeb32c0f381dacf
blucheez <nathanalam99@gmail.com>
2015-08-03 11:10:46 -0400

    Revert "score does not change"
    This reverts commit e0059c2fbddfce0a9def9f3064fd4d0e6a655248.

--- BumperPanel.java
+++ BumperPanel.java
@@ -6,197 +6,176 @@ import java.awt.image.*;
 
 public class BumperPanel extends JPanel {
 
-    public static int lives = 3;
-    private static int xFRAME;
-    private static int yFRAME;
-    private static final Color BACKGROUND = Color.BLACK;
-    private static final Color FOOD_COLOR = Color.YELLOW;
-    private static final Color BUMPER_COLOR = Color.BLUE;
-    private int slowCount = 0;
-    private int deathCount = 0;
-    private Polkadot[] pd;
+   public static int lives = 3;
+   private static int xFRAME;
+   private static int yFRAME;
+   private static final Color BACKGROUND = Color.BLACK;
+   private static final Color FOOD_COLOR = Color.YELLOW;
+   private static final Color BUMPER_COLOR = Color.BLUE;
+   private int slowCount = 0;
+   private Polkadot[] pd;
 
-    private BufferedImage myImage;
-    private Graphics myBuffer;
-    private int score;
-    private Timer refresher;
-    private Pacman pacman;
-    private Ghost[] ghosts;
-    private Color[][] bluePixels;
-    private Map map;
-    private DotProcess dotprocessor;
+   private BufferedImage myImage;
+   private Graphics myBuffer;
+   private int score;
+   private Timer refresher;
+   private Pacman pacman;
+   private Ghost[] ghosts;
+   private Color[][] bluePixels;
+   private Map map;
+   private DotProcess dotprocessor;
 
-    public BumperPanel(int x, int y) {
-        xFRAME = x;
-        yFRAME = y;
-        myImage = new BufferedImage(xFRAME, yFRAME, BufferedImage.TYPE_INT_RGB);
-        myBuffer = myImage.getGraphics();
-        map = new Map();
-        bluePixels = map.getArray();
-        pacman = new Pacman(200, 200);
-        dotprocessor = new DotProcess(xFRAME, yFRAME);
-
-        pd = new Polkadot[20000];
-        for (int i = 0; i < pd.length; i++) {
-            pd[i] = new Polkadot();
-        }
-        dotprocessor.placeDots(pd, bluePixels);
-
-        ghosts = new Ghost[5];
-        for (int i = 0; i < ghosts.length; i++) {
-            ghosts[i] = new Ghost();
-        }
+   public BumperPanel(int x, int y) {
+      xFRAME = x;
+      yFRAME = y;
+      myImage = new BufferedImage(xFRAME, yFRAME, BufferedImage.TYPE_INT_RGB);
+      myBuffer = myImage.getGraphics();
+      map = new Map();
+      bluePixels = map.getArray();
+      pacman = new Pacman(200, 200);
+      dotprocessor = new DotProcess(xFRAME, yFRAME);
+      
+      pd = new Polkadot[20000];
+      for (int i = 0; i < pd.length; i++) {
+         pd[i] = new Polkadot();
+      }
+      dotprocessor.placeDots(pd, bluePixels);
+   
+      ghosts = new Ghost[5];
+      for (int i = 0; i < ghosts.length; i++) {
+         ghosts[i] = new Ghost();
+      }
    // ghosts[1] = new Ghost(260, 140);
-        //   	ghosts[2] = new Ghost(240, 160);
-        //   	ghosts[3] = new Ghost(250, 150);
-        //   	ghosts[0] = new Ghost(240, 140);
+      //   	ghosts[2] = new Ghost(240, 160);
+      //   	ghosts[3] = new Ghost(250, 150);
+      //   	ghosts[0] = new Ghost(240, 140);
+   
+      refresher = new Timer(1, new Refresh());
+   
+      refresher.start();
+      setFocusable(true);
+      addKeyListener(new Key());
+   
+   }
 
-        refresher = new Timer(1, new Refresh());
+   public static void setLives(int x) {
+      //x = Integer.parseInt(JOptionPane.showInputDialog("Set Lives: "));
+      lives = x;
+   }
 
-        refresher.start();
-        setFocusable(true);
-        addKeyListener(new Key());
+   public static int getLives() {
+      return lives;
+   }
 
-    }
+   public void paintComponent(Graphics g) {
+      g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
+   }
 
-    public static void setLives(int x) {
-        //x = Integer.parseInt(JOptionPane.showInputDialog("Set Lives: "));
-        lives = x;
-    }
+   public class Key extends KeyAdapter {
+   
+      public void keyPressed(KeyEvent e) {
+         //System.out.println(e.getKeyCode());
+         if ((e.getKeyCode() == KeyEvent.VK_UP) && (pacman.getY() > 15)) {
+            pacman.moveVertical(-5);
+         }
+         if ((e.getKeyCode() == KeyEvent.VK_DOWN) && (pacman.getY() < 285)) {
+            pacman.moveVertical(5);
+         }
+         if ((e.getKeyCode() == KeyEvent.VK_RIGHT) && (pacman.getX() < 485)) {
+            pacman.moveHorizontal(5);
+         }
+         if ((e.getKeyCode() == KeyEvent.VK_LEFT) && (pacman.getX() > 15)) {
+            pacman.moveHorizontal(-5);
+         }
+      }
+   }
 
-    public static int getLives() {
-        return lives;
-    }
-
-    public void paintComponent(Graphics g) {
-        g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
-    }
-
-    public class Key extends KeyAdapter {
-
-        public void keyPressed(KeyEvent e) {
-            //System.out.println(e.getKeyCode());
-            if ((e.getKeyCode() == KeyEvent.VK_UP) && (pacman.getY() > 15)) {
-                pacman.moveVertical(-5);
-            }
-            if ((e.getKeyCode() == KeyEvent.VK_DOWN) && (pacman.getY() < 285)) {
-                pacman.moveVertical(5);
-            }
-            if ((e.getKeyCode() == KeyEvent.VK_RIGHT) && (pacman.getX() < 485)) {
-                pacman.moveHorizontal(5);
-            }
-            if ((e.getKeyCode() == KeyEvent.VK_LEFT) && (pacman.getX() > 15)) {
-                pacman.moveHorizontal(-5);
-            }
-        }
-    }
-
-    private class Refresh implements ActionListener {
-
-        public void actionPerformed(ActionEvent e) {
-            myBuffer.setColor(Color.black);
-            myBuffer.fillRect(0, 0, xFRAME, yFRAME);
-
-            myBuffer.drawImage(map.getImage(), 0, 0, xFRAME, yFRAME, null);
-
-            pacman.draw(myBuffer);
-            WallCollision.checkNorth(pacman, bluePixels);
-            WallCollision.checkEast(pacman, bluePixels);
-            WallCollision.checkSouth(pacman, bluePixels);
-            WallCollision.checkWest(pacman, bluePixels);
-
-            if (slowCount == 7) {
-                for (int i = 0; i < ghosts.length; i++) {
-                    ghosts[i].draw(myBuffer);
-                    WallCollision.checkNorth(ghosts[i], bluePixels);
-                    WallCollision.checkEast(ghosts[i], bluePixels);
-                    WallCollision.checkSouth(ghosts[i], bluePixels);
-                    WallCollision.checkWest(ghosts[i], bluePixels);
-                    ghosts[i].move(5);
-                    if (DeathByGhost.collide(pacman, ghosts[i])) {
-                        refresher.stop();
-                        lives--;
-                        pacman.setX(200);
-                        pacman.setY(200);
-                        for (int q = 0; q < ghosts.length; q++) {
-                            ghosts[i].setX(240);
-                            ghosts[i].setY(140);
-                        }
-                        GameOver.endGame();
-                    }
-                }
-                slowCount = 0;
-            }
-            slowCount++;
-
+   private class Refresh implements ActionListener {
+   
+      public void actionPerformed(ActionEvent e) {
+         myBuffer.setColor(Color.black);
+         myBuffer.fillRect(0, 0, xFRAME, yFRAME);
+      
+         myBuffer.drawImage(map.getImage(), 0, 0, xFRAME, yFRAME, null);
+      
+         pacman.draw(myBuffer);
+         WallCollision.checkNorth(pacman, bluePixels);
+         WallCollision.checkEast(pacman, bluePixels);
+         WallCollision.checkSouth(pacman, bluePixels);
+         WallCollision.checkWest(pacman, bluePixels);
+      
+         if (slowCount == 7) {
             for (int i = 0; i < ghosts.length; i++) {
-                ghosts[i].draw(myBuffer);
+               ghosts[i].draw(myBuffer);
+               WallCollision.checkNorth(ghosts[i], bluePixels);
+               WallCollision.checkEast(ghosts[i], bluePixels);
+               WallCollision.checkSouth(ghosts[i], bluePixels);
+               WallCollision.checkWest(ghosts[i], bluePixels);
+               ghosts[i].move(5);
+               if (DeathByGhost.collide(pacman, ghosts[i])) {
+                  refresher.stop();
+                  GameOver.endGame();
+               }
             }
-
-            myBuffer.setColor(Color.black);
-            myBuffer.fillRect(0, 0, xFRAME, yFRAME);
-
-            myBuffer.drawImage(map.getImage(), 0, 0, xFRAME, yFRAME, null);
-
-            pacman.draw(myBuffer);
-            WallCollision.checkNorth(pacman, bluePixels);
-            WallCollision.checkEast(pacman, bluePixels);
-            WallCollision.checkSouth(pacman, bluePixels);
-            WallCollision.checkWest(pacman, bluePixels);
-
-            if (slowCount == 7) {
-                for (int i = 0; i < ghosts.length; i++) {
-                    ghosts[i].draw(myBuffer);
-                    WallCollision.checkNorth(ghosts[i], bluePixels);
-                    WallCollision.checkEast(ghosts[i], bluePixels);
-                    WallCollision.checkSouth(ghosts[i], bluePixels);
-                    WallCollision.checkWest(ghosts[i], bluePixels);
-                    ghosts[i].move(1);
-                    if (DeathByGhost.collide(pacman, ghosts[i])) {
-                        refresher.stop();
-                        lives--;
-                        pacman.setX(200);
-                        pacman.setY(200);
-
-                        for (int q = 0; q < ghosts.length; q++) {
-                            ghosts[q].setX(240);
-                            ghosts[q].setY(140);
-                        }
-
-                        if (lives == 0) {
-                            GameOver.endGame();
-                        }
-                        refresher.start();
-                    }
-                }
-                slowCount = 0;
-            }
-            slowCount++;
-
+            slowCount = 0;
+         }
+         slowCount++;
+      
+         for (int i = 0; i < ghosts.length; i++) {
+            ghosts[i].draw(myBuffer);
+         }
+      
+         myBuffer.setColor(Color.black);
+         myBuffer.fillRect(0, 0, xFRAME, yFRAME);
+      
+         myBuffer.drawImage(map.getImage(), 0, 0, xFRAME, yFRAME, null);
+      
+         pacman.draw(myBuffer);
+         WallCollision.checkNorth(pacman, bluePixels);
+         WallCollision.checkEast(pacman, bluePixels);
+         WallCollision.checkSouth(pacman, bluePixels);
+         WallCollision.checkWest(pacman, bluePixels);
+      
+         if (slowCount == 7) {
             for (int i = 0; i < ghosts.length; i++) {
-                ghosts[i].draw(myBuffer);
+               ghosts[i].draw(myBuffer);
+               WallCollision.checkNorth(ghosts[i], bluePixels);
+               WallCollision.checkEast(ghosts[i], bluePixels);
+               WallCollision.checkSouth(ghosts[i], bluePixels);
+               WallCollision.checkWest(ghosts[i], bluePixels);
+               ghosts[i].move(1);
+               if (DeathByGhost.collide(pacman, ghosts[i])) {
+                  refresher.stop();
+                  GameOver.endGame();
+               }
             }
-
-            for (int i = 0; i < pd.length; i++) {
-                if (PolkadotCollision.collide(pacman, pd[i])) {
-                    score++;
-                    pd[i].setY(pd[i].getY() + 300);
-
-                }
+            slowCount = 0;
+         }
+         slowCount++;
+      
+         for (int i = 0; i < ghosts.length; i++) {
+            ghosts[i].draw(myBuffer);
+         }
+         
+         for (int i=0; i<pd.length; i++)
+            if (PolkadotCollision.collide(pacman, pd[i]))
+            {
+               score++;
+               pd[i].setY(pd[i].getY()+300);
             }
-
-            dotprocessor.drawDots(pd, myBuffer);
-
-            myBuffer.setColor(Color.white);
-            myBuffer.setFont(new Font("Monospaced", Font.BOLD, 8));
-            myBuffer.drawString("Lives:" + lives, 430, 60);
-
-            myBuffer.setColor(Color.white);
-            myBuffer.setFont(new Font("Monospaced", Font.BOLD, 8));
-            myBuffer.drawString("Score:" + score, 40, 60);
-
-            repaint();
-        }
-    }
+         
+         dotprocessor.drawDots(pd, myBuffer);
+      
+         myBuffer.setColor(Color.white);
+         myBuffer.setFont(new Font("Monospaced", Font.BOLD, 8));
+         myBuffer.drawString("Lives:" + lives, 430, 60);
+            
+         myBuffer.setColor(Color.white);
+         myBuffer.setFont(new Font("Monospaced", Font.BOLD, 8));
+         myBuffer.drawString("Score:" + score, 40, 60);
+                     
+         repaint();
+      }
+   }
 
 }
--- GameOver.java
+++ GameOver.java
@@ -1,44 +1,51 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GameOver extends JPanel {
-
-    public static JFrame endFrame;
-
-    public static void endGame() {
-        endFrame = new JFrame("OH NO!");
-        endFrame.setSize(400, 400);
-        endFrame.setLocation(200, 200);
-        endFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        endFrame.setContentPane(new GameOver());
-        endFrame.setVisible(true);
-    }
-
-    public GameOver() {
-        setLayout(new GridLayout(2, 1));
-
-        JLabel label = new JLabel("Game Over!", SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, 50));
-        add(label);
-
-        JButton button = new JButton("Start Again");
-        button.setBackground(Color.BLUE);
-        button.addActionListener(new Listener());
-        button.setFont(new Font("Arial", Font.BOLD, 20));
-        button.setForeground(Color.white);
-        add(button);
-    }
-
-    private class Listener implements ActionListener {
-
-        public void actionPerformed(ActionEvent e) {
-
+public class GameOver extends JPanel{
+   public static JFrame endFrame;
+   public static void endGame() {
+      endFrame = new JFrame("OH NO!");
+      endFrame.setSize(400, 400);
+      endFrame.setLocation(200, 200);
+      endFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      endFrame.setContentPane(new GameOver());
+      endFrame.setVisible(true);
+   }
+   
+   public GameOver() {
+      setLayout(new GridLayout(2, 1));
+    
+      JLabel label = new JLabel("You lost a life!", SwingConstants.CENTER);
+      label.setFont(new Font("Arial", Font.BOLD, 50));
+      add(label);
+     	 
+      JButton button = new JButton("Start Again");
+      button.setBackground(Color.BLUE);
+      button.addActionListener(new Listener());
+      button.setFont(new Font("Arial", Font.BOLD, 20));
+      button.setForeground(Color.white);
+      add(button);
+   }
+   
+   private class Listener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         if (BumperPanel.getLives()>1)
+         {
+            BumperPanel.setLives(BumperPanel.getLives()-1);
             GameOver.endFrame.setVisible(false);
-            Driver.frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+            Driver.frame.setVisible(false);
             Driver.gameFrame();
-
-        }
-    }
+         }
+         else
+         {
+            BumperPanel.setLives(3);
+            GameOver.endFrame.setVisible(false);
+            Driver.frame.setVisible(false);
+            Start.menuFrame.setVisible(true);
+         }
+      }
+   }
 }
+
+
